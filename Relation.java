5754437518fa33845a9d0821c63da8d8340cc315
Ian Fridge <enkie1985@gmail.com>
2013-03-15 20:18:34 -0700

    Made database wrapper

--- Relation.java
+++ Relation.java
@@ -1,29 +1,66 @@
-import com.sleepycat.db.Cursor;
+import java.util.Iterator;
 
-public class Relation {
+import com.sleepycat.db.Cursor;
+import com.sleepycat.db.DatabaseException;
+import com.sleepycat.db.DatabaseEntry;
+import com.sleepycat.db.Database;
+import com.sleepycat.db.LockMode;
+import com.sleepycat.db.OperationStatus;
+
+
+public class Relation implements Iterator {
+
     Database db;
     bool isIndex;
+    Cursor relCursor = null;
+    int listIndex = 0;
+    OperationStatus ret;
+    DatabaseEntry currentEntry = new DatabaseEntry();
+    DatabaseEntry currentKey = new DatabaseEntry();
+    ArrayList<Object> internalList;
+
     public Relation(Database db) {
         this.db = db;
     }
 
-    public Relation(Database db, bool isIndex) {
+    public Relation(Database db, bool isIndex, bool isIntermediate) {
         this.db = db;
         this.isIndex = isIndex;
-
+        this.isIntermediate = isIntermediate;
     }
 
-    public join(Relation other){
-        // if other.isIndex
-        //      other_cursor = other.getCursor()
+    public void setCursor(){
+        if(isIndex) {
+            this.relCursor = this.db.openCursor(null, null);
+            this.relCursor.getFirst(this.currentKey, this.currentEntry, LockMode.DEFAULT)
+        } else {
+            this.attrIndex = 0;
+        }
     }
 
-    public Cursor getCursor(){
-        return db.openCursor(null, null);
+    public bool hasNext() {
+        if(this.isIndex) {
+            this.relCursor.getNext(this.currentKey, this.currentEntry, LockMode.DEFAULT);
+            return true;
+        } else if(this.isIntermediate) {
+            if(this.listIndex < this.internalList.size()) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public getDb(){
-        return db;
+    public Object next() {
+        if(this.isIndex) {
+            if(this.relCursor == null) {
+                this.setCursor();
+            }
+            return this.currentEntry;
+        }
+        return this.internalList.get(this.listIndex++);
     }
 
+    public void remove() {
+        throw new UnsupportedOperationException("Not Implemented");
+    }
 }
